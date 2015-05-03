8ef6f69433389971d7dd0247373c96ff3bf5ca1c
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-05-03 00:07:37 -0400

    basic lemma dict created

--- transform_greek_lemmata.py
+++ transform_greek_lemmata.py
@@ -1,31 +1,69 @@
+from cltk.corpus.greek.beta_to_unicode import Replacer
+from collections import  defaultdict
+
+replacer = Replacer()
 
 
 def file_line_generator(file):
+    """Open file line-by-line"""
     with open(file) as file_opened:
-        for line in file_opened:
-            yield line[:-1]  # remove '\n' from end of each line
+        for file_line in file_opened:
+            yield file_line[:-1]  # remove '\n' from end of each line
 
 
-if __name__ == '__main__':
-    file_generator = file_line_generator('greek-lemmata.txt')
+def parse_perseus_lemmata_file(file_generator, greek):
+    """Parse lemmata file, looping through string for all data."""
+    count = 0
     for line in file_generator:
-        #line = raw_line[:-1]
+        count += 1
+        if count % 1000 == 0:
+            print('Parsing line {0}'.format(count))
         line_split = line.split('\t')
         headword = line_split[0]
         headword_id = line_split[1]
-        print(headword, headword_id)
-        #print('RAW LINE:', line)
         line_lemmata = line_split[2:]
         for lemma_str in line_lemmata:
-            #print('RAW LEMMA:', lemma_str)
             lemma_list = lemma_str.split(' ', 1)
             lemma = lemma_list[0]
             lemma_pos_str = lemma_list[1]
             lemma_pos_list = lemma_pos_str.split(') (')
 
-            #for lemma_pos in lemma_pos_list:
+            for lemma_pos in lemma_pos_list:
+                # rm initial paren
+                if lemma_pos.startswith('('):
+                    lemma_pos = lemma_pos[1:]
 
-            #print(lemma)
-            print(lemma_pos_list)
+                # rm final paren
+                if lemma_pos.endswith(')'):
+                    lemma_pos = lemma_pos[:-1]
 
-        input()
+                # break out any dialect or extra data: '(epic doric ionic aeolic)', '(adverb)'
+                if '(' in lemma_pos and lemma_pos.endswith(')'):
+                    lemma_pos_list = lemma_pos.split(' (')
+                    lemma_pos = lemma_pos_list[0]
+                    lemma_pos_comment = lemma_pos_list[1][:-1]
+                    lemma_pos_comment_list = lemma_pos_comment.split(' ')
+
+                if greek:
+                    lemma = replacer.beta_code(lemma.upper() + ' ')[:-1].lower()  # add space to get final sigma 'ς', then rm it #? why some coming out capitalized?
+                    headword = replacer.beta_code(headword.upper() + ' ')[:-1].lower()  # add space to get final sigma 'ς', then rm it
+
+                #print(lemma, headword)
+
+                yield lemma, headword
+
+
+if __name__ == '__main__':
+    lemma_headword_map = {}
+    greek_file_generator = file_line_generator('greek-lemmata.txt')
+    greek_lemma_headword = parse_perseus_lemmata_file(greek_file_generator, greek=False)
+
+    print('Starting to build map …')
+    lemmata_dd = defaultdict(list)
+    for k, v in greek_lemma_headword:
+        lemmata_dd[k].append(v)
+
+    print('Starting to write file …')
+    with open('greek_lemmata_cltk.txt', 'w') as file_opened:
+        file_opened.write(str(dict(lemmata_dd)))
+
